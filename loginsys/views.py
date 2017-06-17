da43a598c0be0ce285498dd45940b43ef67adc10
brainiaconly <maxonly9@gmail.com>
2017-06-17 18:45:36 +0300

    - implement registration

--- loginsys/views.py
+++ loginsys/views.py
@@ -16,7 +16,8 @@ def login(request):
         password = request.POST.get('password','')
         user = auth.authenticate(username=username,password=password)
         if user is not None:
-            auth.login(request,user)
+            if user.last_name != 'True':
+                auth.login(request,user)
             return redirect('/')
         with DataManager() as dm:
             hash = dm.get_password(username)
@@ -49,10 +50,21 @@ def register(request):
     if request.POST:
         newuser_form = UserCreationForm(request.POST)
         if newuser_form.is_valid():
-            newuser_form.save()
-            newuser = auth.authenticate(username=newuser_form.cleaned_data['username'],password=newuser_form.cleaned_data['password2'])
-            auth.login(request,newuser)
+            username = newuser_form.cleaned_data['username']
+            email = request.POST.get('Email','')
+            password = newuser_form.cleaned_data['password2']
+            foundUsernames = len(User.objects.filter(username=username))
+            foundEmails = len(User.objects.filter(email=email))
+            print 'username exists {0}, email exists {1}'.format(foundUsernames, foundEmails)
+            if foundUsernames == 0 and foundEmails == 0:
+                new_user = User.objects.create_user(username, email, password)
+                new_user.last_name = 'True'
+                new_user.is_staff = True
+                new_user.save()
+            #newuser = auth.authenticate(username=newuser_form.cleaned_data['username'],password=newuser_form.cleaned_data['password2'])
+            #auth.login(request,newuser)
             return redirect('/')
         else:
+            print 'form is not valid'
             args['form'] = newuser_form
     return render_to_response('register.html',args)
